aa2006214420b1b2fbf2f8801fa4d52dc265d69b
Crizalid <60556466+crizalid12@users.noreply.github.com>
2021-07-27 11:04:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+def find_even_index(arr):
+    first = arr[0]
+    last = arr[-1]
+    index = 0
+    if sum(arr) == 0:
+        index = 0
+        return index,  "Should pick the first index if more cases are valid"
+    if first == sum(arr[1:]):
+        index = 0
+    if last == sum(arr[:-1]):
+        index = last
+        return len(arr) - 1
+    for i in range(1, len(arr)):
+        if sum(arr[:i]) == sum(arr[-1:i:-1]):
+            index = i
+            break
+        else:
+            index = -1
+    return index
